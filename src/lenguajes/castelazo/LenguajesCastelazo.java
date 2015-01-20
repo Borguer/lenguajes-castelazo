b8b5eb0cf58735e48f0faeb0bb14a5b86fe52000
T-107 <T-107@PC246>
2015-01-19 19:29:03 -0600

    Mi primer push a mi repositorio

---
+++ src/lenguajes/castelazo/LenguajesCastelazo.java
@@ -0,0 +1,14 @@
+
+package lenguajes.castelazo;
+import java.io.*;
+import java.util.Scanner;
+
+public class LenguajesCastelazo {
+    
+
+   
+    public static void main(String[] args) {
+        System.out.println("Mi primer porgrama de Java 2");
+    }
+    
+}
